37d904db900e1a94d62d26b973c774ce8279c2de
gaboquerizo <gabrielbaquerizo2000@gmail.com>
2024-05-26 04:21:22 -0500

    Changes and improvements made to the structure and interface + Dark mode

---
+++ index.js
@@ -0,0 +1,6 @@
+
+/* Variables */
+const btnChangeMode = document.querySelector('#change-mode');
+
+/* Modo Oscuro */
+btnChangeMode.addEventListener('click', () => document.body.classList.toggle('dark'));
