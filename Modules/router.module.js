83df7b4cfe61e66889a01b5efe166d26f5d3a1a4
gaboquerizo <gabrielbaquerizo2000@gmail.com>
2024-09-13 09:34:15 -0500

    URL Routing test

--- Modules/router.module.js
+++ Modules/router.module.js
@@ -25,7 +25,8 @@ class Router {
     nav(route, addToHistory = true) {
 
         const routing = new NavMenu;
-        const path = window.location.pathname.slice(0, -1);
+        // const path = window.location.pathname.slice(0, -1);
+        const path = '/Components_ui'
         const routePath = path + route;
 
         console.log(routePath);
